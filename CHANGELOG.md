816b9a513027888aa49fa69092a7f805b0d3039f
LiosK <contact@mail.liosk.net>
2021-03-09 22:40:55 +0900

    Mark `precision` parameter of `fromNumber()` as deprecated because the argument needs to be determined based on highly implementation-specific details.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## Unreleased
+
+### Deprecated
+
+- `precision` parameter of `fromNumber()` because the argument needs to be
+  determined based on highly implementation-specific details.
+
 ## v1.0.0 - 2021-03-09
 
 ### Added
--- src/index.ts
+++ src/index.ts
@@ -178,6 +178,9 @@ export class BigAmount {
    * Creates a [[BigAmount]] from `number`. Unlike [[BigAmount.create]], this
    * method finds a rational approximate of a non-integral number.
    *
+   * @param precision - _Deprecated._ This parameter may be removed or replaced
+   *        in the future because the argument needs to be determined based on
+   *        highly implementation-specific details.
    * @category Instance Creation
    */
   static fromNumber(x: number, precision = 100_000_000): BigAmount {
